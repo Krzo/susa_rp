fdbd1d4a0b4ac8b6885247c049d10961f650fc9e
Osiimov <oshan-57@hotmail.com>
2016-04-08 09:10:01 +0200

    Added files via upload

--- susa_server/jobs_s.lua
+++ susa_server/jobs_s.lua
@@ -2,13 +2,23 @@
 	function saving(p,typ,money)
 		local username = getElementData(p,"username")
 		if typ == 1 then
-			local f = dbQuery(db, "UPDATE accounts SET d_licence=?, money=? WHERE username=?",1 )
+			dbQuery(db, "UPDATE accounts SET d_licence=?, money=? WHERE username=?",1 )
 			setPlayerMoney(p,money)
-			dbFree(f)
 		elseif typ == 2 then
-			local f = dbQuery(db, "UPDATE accounts SET b_licence=?,money=? WHERE username=?",1)
+			dbQuery(db, "UPDATE accounts SET b_licence=?,money=? WHERE username=?",1)
 			setPlayerMoney(p,money)
-			dbFree(f)
+		elseif typ == 3 then
+			dbQuery(db, "UPDATE accounts SET taxi=?,money=? WHERE username=?",1)
+			setPlayerMoney(p,money)
+		elseif typ == 4 then
+			dbQuery(db, "UPDATE accounts SET bus=?,money=? WHERE username=?",1)
+			setPlayerMoney(p,money)
+		elseif typ == 5 then
+			dbQuery(db, "UPDATE accounts SET trash=?,money=? WHERE username=?",1)
+			setPlayerMoney(p,money)
+		elseif typ == 6 then
+			dbQuery(db, "UPDATE accounts SET tuning=?,money=? WHERE username=?",1)
+			setPlayerMoney(p,money)
 		end	
 	end
 	addEvent("licence",true)
