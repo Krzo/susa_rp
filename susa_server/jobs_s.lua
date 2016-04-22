546d85bcda7107012b68329101e6b9a98bc1f631
Krzo <krz0designs@gmail.com>
2016-04-22 08:09:29 +0200

    Added files via upload

--- susa_server/jobs_s.lua
+++ susa_server/jobs_s.lua
@@ -2,21 +2,27 @@
 	function saving(p,typ,money)
 		local username = getElementData(p,"username")
 		if typ == 1 then
+			setElementData(p,"susa:d_licence",1)
 			local f = dbQuery(db, "UPDATE accounts SET d_licence=?, money=? WHERE username=?",1 )
 			dbFree(f)
 		elseif typ == 2 then
+			setElementData(p,"susa:b_licence",1)
 			local f = dbQuery(db, "UPDATE accounts SET b_licence=?,money=? WHERE username=?",1)
 			dbFree(f)
 		elseif typ == 3 then
+			setElementData(p,"susa:taxi",1)
 			local f = dbQuery(db, "UPDATE accounts SET taxi_licence=?,money=? WHERE username=?",1)
 			dbFree(f)
 		elseif typ == 4 then
+			setElementData(p,"susa:trash",1)
 			local f = dbQuery(db, "UPDATE accounts SET bus_licence=?,money=? WHERE username=?",1)
 			dbFree(f)
 		elseif typ == 5 then
+			setElementData(p,"susa:bus",1)
 			local f = dbQuery(db, "UPDATE accounts SET trashman_licence=?,money=? WHERE username=?",1)
 			dbFree(f)
 		elseif typ == 6 then
+			setElementData(p,"susa:tuning",1)
 			local f = dbQuery(db, "UPDATE accounts SET tuning_licence=?,money=? WHERE username=?",1)
 			dbFree(f)
 		end
